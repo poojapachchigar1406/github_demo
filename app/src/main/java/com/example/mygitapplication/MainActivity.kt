6f49b721273f620059bca38d5e66d6cc0fad3c1d
pooja-cts <pooja.pachchigar@cognizant.com>
2023-06-09 10:17:45 -0400

    This is my first commit

--- app/src/main/java/com/example/mygitapplication/MainActivity.kt
+++ app/src/main/java/com/example/mygitapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.mygitapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showToast()
+    }
+
+    private fun showToast() {
+        Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show()
     }
 }
